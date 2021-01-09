b9fe1da8abc913b25f715c1ee5702e7fc7b2d3cc
benoit barges <benoitbarges@gmail.com>
2021-01-09 08:51:02 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Todo.destroy_all
+
+User.all.each do |user|
+  10.times do
+    Todo.create!(title: Faker::Lorem.sentence(word_count: 4), user_id: user.id)
+  end
+end
