73261fca6592c3d680cf506d9617110c176a66f6
benoit barges <benoitbarges@gmail.com>
2021-01-09 14:43:56 +0100

    handle console error about non unique ids

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,11 +26,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-import "../plugins/init_flatpickr";
+import { initFlatpickr } from "../plugins/init_flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initFlatpickr()
 });
 
 import "controllers"
--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,32 +1,38 @@
 import flatpickr from "flatpickr";
 import Rails from "@rails/ujs"
 
-flatpickr(".datepicker", {
-  "locale": {
-      "firstDayOfWeek": 1 // start week on Monday
-  },
-  minDate: "today",
-  altInput: true
-});
 
-const selectDate = (selectedDates, dateStr, instance) => {
-  let data =  new FormData()
-  data.append('deadline', dateStr)
-  let id = instance.element.dataset.id
+const initFlatpickr = () => {
 
-  Rails.ajax({
-      url: `/todos/${id}/update_deadline`,
-      type: 'PATCH',
-      data: data
-    })
+  flatpickr(".datepicker", {
+    "locale": {
+        "firstDayOfWeek": 1 // start week on Monday
+    },
+    minDate: "today",
+    altInput: true
+  });
+
+  const selectDate = (selectedDates, dateStr, instance) => {
+    let data =  new FormData()
+    data.append('deadline', dateStr)
+    let id = instance.element.dataset.id
+
+    Rails.ajax({
+        url: `/todos/${id}/update_deadline`,
+        type: 'PATCH',
+        data: data
+      })
+  }
+
+  flatpickr(".changeDate", {
+    "locale": {
+        "firstDayOfWeek": 1 // start week on Monday
+    },
+    minDate: "today",
+    altInput: true,
+    onClose: selectDate
+  });
 }
 
-flatpickr(".changeDate", {
-  "locale": {
-      "firstDayOfWeek": 1 // start week on Monday
-  },
-  minDate: "today",
-  altInput: true,
-  onClose: selectDate
-});
+export { initFlatpickr }
 
