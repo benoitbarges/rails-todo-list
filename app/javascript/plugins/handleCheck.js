f7595e8d5c06b94a4f7608e0491daae57c4651d3
benoit barges <benoitbarges@gmail.com>
2021-01-17 10:25:12 +0100

    checkbox effect

--- app/javascript/plugins/handleCheck.js
+++ app/javascript/plugins/handleCheck.js
@@ -10,12 +10,14 @@ const handleCheck = () => {
       const todoTitle = document.querySelector(`#todo-title-${id}`)
       const faCheck = document.querySelector(`.fa-check-${id}`)
       const wellDone = "<p class='well-done my-auto'>WELL DONE !</p>"
+      const checkCircle = checkbox.nextElementSibling
 
       if (checkbox.classList.contains('checked')) {
         todoTitle.classList.remove('todo-title-checked')
         checkbox.classList.remove('checked')
         faCheck.classList.remove('fa-check-checked')
         checkbox.parentNode.querySelector('.well-done')?.remove()
+        checkCircle.classList.remove('check-circle-on')
 
         Rails.ajax({
           url: `/todos/${id}/mark_as_not_done`,
@@ -28,6 +30,7 @@ const handleCheck = () => {
         faCheck.classList.add('fa-check-checked')
 
         checkbox.parentNode.insertAdjacentHTML('afterbegin', wellDone)
+        checkCircle.classList.add('check-circle-on')
 
         Rails.ajax({
           url: `/todos/${id}/mark_as_done`,
